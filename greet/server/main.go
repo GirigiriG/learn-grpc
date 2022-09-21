fd5ac780eb5e797b5db7226ce9dd27a6caa04976
Gideon Girigiri <gideongirigiri@gamil.com>
2022-09-20 22:43:53 -0400

    created grpc server | next | work on client grpc

--- greet/server/main.go
+++ greet/server/main.go
@@ -1,25 +1,40 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
-	pb "github.com/GirigiriG/learn-grpc/greet/proto"
+	pb "github.com/GirigiriG/learn-grpc/greet/proto/greet/proto"
+	"google.golang.org/grpc"
 )
 
 const (
 	GRPCServerAddress = "0.0.0.0:5051"
 )
 
-type server struct {
-	pb.GreetServiceServer
+type GreetServer struct {
+	pb.UnimplementedGreetServiceServer
+}
+
+func (s *GreetServer) sayHello(ctx context.Context, in *pb.GreetRequest) *pb.GreetResponse {
+	log.Printf("recieved %v", in.GetFirstName())
+	var name string = "Gideon from grpc"
+	return &pb.GreetResponse{Result: name}
 }
 
 func main() {
-	listen, err := net.Listen("tcp", GRPCServerAddress)
+	grpcServerListner, err := net.Listen("tcp", GRPCServerAddress)
 	if err != nil {
 		log.Printf("Failed to connect to grpc server %v \n", GRPCServerAddress)
 		return
 	}
 
+	s := grpc.NewServer()
+	pb.RegisterGreetServiceServer(s, &GreetServer{})
+	
+
+	//start the server
+
+	s.Serve(grpcServerListner)
 }
